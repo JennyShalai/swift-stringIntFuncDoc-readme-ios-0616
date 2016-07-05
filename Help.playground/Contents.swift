3a576e0530a917dac6dd9e7cabd3715c79720dd3
JennyShalai <jenny.shalai@gmail.com>
2016-07-05 17:14:45 -0400

    done

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -4,3 +4,6 @@ name.uppercaseString
 let uppercaseName = name.uppercaseString
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+let backToLowercaseName = uppercaseName.lowercaseString
+print(backToLowercaseName)
